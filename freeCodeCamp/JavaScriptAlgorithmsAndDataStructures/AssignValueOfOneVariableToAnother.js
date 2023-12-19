33fda2adb0c908b38142258e37eb7210c811ea3c
Samuel Lee <slee0258@gmail.com>
2023-12-19 21:04:04 +0000

    Reduce num for leetcode problem and practice problems for freeCodeCamp.

---
+++ freeCodeCamp/JavaScriptAlgorithmsAndDataStructures/AssignValueOfOneVariableToAnother.js
@@ -0,0 +1,9 @@
+// after a value is assigned to a variable, you can assign the value to another variable using assignment operator
+
+// declare myVar variable and assign it the value '5', then assign the value of the variable myVar to myNum.
+/*
+  let myVar;
+  myVar = 5;
+  let myNum;
+  myNum = myVar;
+*/
