b3b89835274c3a2e8139261567abd9f3534a12ce
Samuel Lee <slee0258@gmail.com>
2023-12-21 03:10:20 +0000

    freeCodeCamp practice problems.

---
+++ freeCodeCamp/JavaScriptAlgorithmsAndDataStructures/divideNumbersWithJS.js
@@ -0,0 +1,3 @@
+// divide one number by another using the '/' symbol so that the quotient equals 2.
+
+// const quotient = 66 / 33;
---
+++ freeCodeCamp/JavaScriptAlgorithmsAndDataStructures/multiplyTwoNumbersWithJS.js
@@ -0,0 +1,3 @@
+// multiply one number by another using the '*' symbol so that the product equals 80.
+
+// const product = 8 * 10;
