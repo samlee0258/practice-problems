8ad091c126178b86eaa9fbf14a42bc9d23e1d0ee
Samuel Lee <slee0258@gmail.com>
2024-01-18 00:14:22 +0000

    freeCodeCamp practice problems.

---
+++ freeCodeCamp/JavaScriptAlgorithmsAndDataStructures/bracketNotationToFindNthToLastCharaInString.js
@@ -0,0 +1,13 @@
+/*
+  You can use the same principle we just used to retrieve the last character in a string to retrieve the Nth-to-last character.
+
+  For example, you can get the value of the third-to-last letter of the const firstName = "Augusta" string by using firstName[firstName.length - 3]
+
+  Use bracket notation to find the second-to-last character in the lastName string.
+*/
+
+/*
+  const lastName = "Lovelace";
+
+  const secondToLastLetterOfLastName = lastName[lastName.length - 2];
+*/
---
+++ freeCodeCamp/JavaScriptAlgorithmsAndDataStructures/nestOneArrayWithinAnother.js
@@ -0,0 +1,13 @@
+/*
+  You can also nest arrays within other arrays, like below:
+
+  const teams = [["Bulls", 23], ["White Sox", 45]];
+
+  This is also called a multi-dimensional array.
+
+  Create a nested array called myArray.
+*/
+
+/*
+  const myArray = [["Array", 1], ["Array", 2]];
+*/
