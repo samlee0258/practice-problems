a0494dc75c1ed408c63d7ddabce65ab8978027db
Samuel Lee <slee0258@gmail.com>
2024-01-18 00:25:49 +0000

    freeCodeCamp manipulate arrays with pop, push, and shift methods.

---
+++ freeCodeCamp/JavaScriptAlgorithmsAndDataStructures/manipulateArraysWithPush.js
@@ -0,0 +1,14 @@
+/*
+  An easy way to append data to the end of an array is via the push() method.
+
+  The push() method takes one or more arguments and appends them to the end of the array, in the order in which they appear.
+  It returns the new length of the array.
+
+  Push ["dog", 3] onto the end of the myArray variable.
+*/
+
+/*
+  const myArray = [["John", 23], ["cat", 2]];
+
+  myArray.push(["dog", 3]);
+*/
