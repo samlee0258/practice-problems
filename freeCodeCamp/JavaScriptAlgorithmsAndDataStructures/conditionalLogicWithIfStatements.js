0f2ccaea0fa7bccdbcd58a5dc9873cc4c79c02d1
Samuel Lee <slee0258@gmail.com>
2024-01-19 00:11:53 +0000

    freeCodeCamp practice problems.

---
+++ freeCodeCamp/JavaScriptAlgorithmsAndDataStructures/conditionalLogicWithIfStatements.js
@@ -0,0 +1,19 @@
+/*
+  if statements are used to make decisions in code.
+  The keyword if tells JavaScript to execute the code in the curly braces under certain conditions, defined in the parentheses.
+  These conditions are known as Boolean conditions and they may only be true or false.
+
+  When the condition evaluates to true, the program executes the statement inside the curly braces.
+  When the Boolean condition evaluates to false, the statement inside the curly braces will not execute.
+
+  Create an if statement inside the function to return Yes, that was true if the parameter wasThatTrue is true and return No, that was false otherwise.
+*/
+
+/*
+  function trueOrFalse(wasThatTrue) {
+    if (wasThatTrue) {
+      return 'Yes, that was true';
+    }
+    return 'No, that was not true';
+  }
+*/
